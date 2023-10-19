cbb632ef66276b87e5d52f2b699c9abc4c27d2e4
Marco-Urbina <136619995+Marco-Urbina@users.noreply.github.com>
2023-10-19 11:32:01 -0600

    update

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   get '/about_me', to: 'users#about_me'
+  resources :skills 
+  resources :projects
 end
